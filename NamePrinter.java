67142e75f979cd43b38172e267f2b85aa34c161f
luca-cataldo <54952305+luca-cataldo@users.noreply.github.com>
2019-12-13 12:25:13 -0500

    Add files via upload

---
+++ NamePrinter.java
@@ -0,0 +1,23 @@
+/*
+Name: NamePrinter
+Purpose: Print my first name using methods.
+Author: Luca Cataldo
+Date: Dec 13, 2019
+*/
+
+import java.io.*;
+
+public class NamePrinter
+{
+    public static void main (String[] args) throws IOException
+    {
+	printName ();
+	printName ();
+	printName ();
+    }
+
+    public static void printName ()
+    {
+	System.out.println ("Luca");
+    }
+}
